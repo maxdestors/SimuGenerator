4963d94b5bd8f314c6ee9329538469a3b7b254c7
frouyer193 <blkdrg123>
2014-12-03 13:44:38 +0100

    Interface de choix de config Chargement des configs dans l'application via un hashmap
     [COMMENT]

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,11 +3,23 @@ package main;
 import feature.Features;
 import generator.simulator.GenerateProjectSimulator;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.io.File;
+import java.nio.channels.FileChannel;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 
 public class Main {
@@ -15,7 +27,76 @@ public class Main {
 	 private static Logger logger = Logger.getLogger("main.Main");
 
 	public static void main(String args[]) {
-	    Logger.getLogger("main").setLevel(Level.ALL);
+	    
+		
+		
+		/*
+		
+		File f = null;
+		File[] paths;
+		Map<String,String> configs = new HashMap<String,String>();
+		f = new File("./config");
+		String fmName = "toto";
+		String FM = fmName+" = FM(A: E F; E: (B|C|D); F: (X|Y)+;)";
+		String configName = "config1";
+		FileChannel in = null; // canal d'entrée
+		FileChannel out = null; // canal de sortie
+
+		//Chargement d'un fichier dans l'application
+		paths = f.listFiles();
+
+		for(File p:paths){
+			System.out.println(p);
+			configs.put(p.getName(), p.getAbsolutePath());
+			System.out.println(p.getName());
+		}
+
+		if(configs.isEmpty()){
+			System.out.println("aucun fichier de configuration n'a été trouvé");
+			//L'idée est d'y proposer d'en rentrer une a la main et de le mettre dans un string
+		}
+		else{
+			
+			//Création de la fenetre de choix des configurations
+			JFrame fenetre = new JFrame();
+			JPanel container = new JPanel();
+			JComboBox combo = new JComboBox();
+			JLabel label = new JLabel("Configuration");
+			JButton button = new JButton("Valider");
+			
+			
+			fenetre.add(container);
+			fenetre.add(combo);
+			fenetre.add(label);  
+			fenetre.setTitle("Simu-generator");
+			fenetre.setSize(250, 150);
+			fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			fenetre.setLocationRelativeTo(null);
+			container.setBackground(Color.white);
+			container.setLayout(new BorderLayout());
+			combo.setPreferredSize(new Dimension(100, 20));
+			JPanel top = new JPanel();
+			top.add(label);
+			top.add(combo);
+			container.add(top, BorderLayout.NORTH);
+			container.add(button, BorderLayout.SOUTH);
+			fenetre.setContentPane(container);
+			fenetre.setVisible(true);    
+			//Ajout des propositions dans la comboBox
+			for(File p:paths){
+				combo.addItem(p.getName());
+			}
+		}
+		
+		
+		
+		
+		*/
+		
+		
+		
+		
+		Logger.getLogger("main").setLevel(Level.ALL);
 		String pathProject = "D:\\Documents\\GitHub\\TP_GL";
 		// Romain // C:\\Documents\\M1\\GL\\TP_GL
 		// Max // D:\\Documents\\GitHub\\TP_GL
